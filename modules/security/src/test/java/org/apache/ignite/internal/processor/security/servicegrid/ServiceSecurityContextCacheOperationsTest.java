65685deadd5b5fce94e4fe28b400961623c790c0
Andrey Kuznetsov <stkuzma@gmail.com>
2019-03-11 19:36:00 +0300

    IMDBGG-1618 Permissions test for cache operations in derived service.

--- modules/security/src/test/java/org/apache/ignite/internal/processor/security/servicegrid/ServiceSecurityContextCacheOperationsTest.java
+++ modules/security/src/test/java/org/apache/ignite/internal/processor/security/servicegrid/ServiceSecurityContextCacheOperationsTest.java
@@ -26,6 +26,8 @@ import org.apache.ignite.plugin.security.SecurityPermissionSet;
 import org.apache.ignite.resources.IgniteInstanceResource;
 import org.apache.ignite.services.Service;
 import org.apache.ignite.services.ServiceContext;
+import org.apache.ignite.testframework.GridTestUtils;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -55,7 +57,7 @@ public class ServiceSecurityContextCacheOperationsTest extends AbstractSecurityT
     /**
      *
      */
-    private static class TestServiceImpl implements TestService {
+    private static class PlainTestServiceImpl implements TestService {
         /** */
         @IgniteInstanceResource
         Ignite ignite;
@@ -147,6 +149,16 @@ public class ServiceSecurityContextCacheOperationsTest extends AbstractSecurityT
     /**
      *
      */
+    @After
+    public void after() throws Exception {
+        stopAllGrids();
+
+        cleanPersistenceDir();
+    }
+
+    /**
+     *
+     */
     @Test
     public void testPlain() throws Exception {
         SecurityPermissionSet permSet0 = builder()
@@ -172,9 +184,9 @@ public class ServiceSecurityContextCacheOperationsTest extends AbstractSecurityT
         cache1.put("kInvokeRemove", "vInvokeRemove");
         cache1.put("kCancelRemove", "vCancelRemove");
 
-        IgniteServices svcs = ignite0.services(ignite0.cluster().forRemotes());
+        IgniteServices svcs = remoteServices(ignite0);
 
-        svcs.deployNodeSingleton(TEST_SERVICE_NAME, new TestServiceImpl());
+        svcs.deployNodeSingleton(TEST_SERVICE_NAME, new PlainTestServiceImpl());
 
         Runnable svcProxy = svcs.serviceProxy(TEST_SERVICE_NAME, Runnable.class, true);
 
@@ -193,5 +205,130 @@ public class ServiceSecurityContextCacheOperationsTest extends AbstractSecurityT
         assertEquals("IMDBGG-1483", "vCancelRemove", cache1.get("kCancelRemove"));
 
         stopAllGrids();
+    }
+
+    /**
+     *
+     */
+    private static class DerivingTestServiceImpl implements TestService {
+        /** */
+        static volatile boolean executed;
+
+        /** */
+        static volatile boolean cancelled;
+
+        /** */
+        @IgniteInstanceResource
+        Ignite ignite;
+
+        /**
+         *
+         */
+        DerivingTestServiceImpl() {
+            executed = cancelled = false;
+        }
+
+        /**
+         *
+         */
+        @Override public void init(ServiceContext ctx) {
+            // No-op.
+        }
+
+        /**
+         *
+         */
+        @Override public void execute(ServiceContext ctx) {
+            remoteServices(ignite).deployNodeSingleton(TEST_SERVICE_NAME, new PlainTestServiceImpl());
+
+            executed = true;
+        }
+
+        /**
+         *
+         */
+        @Override public void cancel(ServiceContext ctx) {
+            remoteServices(ignite).cancelAsync(TEST_SERVICE_NAME).listen(fut -> cancelled = true);
+        }
+
+        /**
+         *
+         */
+        @Override public void run() {
+            Runnable svcProxy = remoteServices(ignite).serviceProxy(TEST_SERVICE_NAME, Runnable.class, true);
+
+            svcProxy.run();
+        }
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void testDerivedService() throws Exception {
+        String derivingTestSrvcName = "derivingTestService";
+
+        SecurityPermissionSet permSet0 = builder()
+            .appendCachePermissions(DEFAULT_CACHE_NAME, CACHE_READ)
+            .appendServicePermissions(derivingTestSrvcName, SERVICE_DEPLOY, SERVICE_INVOKE, SERVICE_CANCEL)
+            .build();
+
+        SecurityPermissionSet permSet1 = builder()
+            .appendCachePermissions(DEFAULT_CACHE_NAME, CACHE_READ, CACHE_PUT, CACHE_REMOVE)
+            .appendServicePermissions(TEST_SERVICE_NAME, SERVICE_DEPLOY, SERVICE_INVOKE, SERVICE_CANCEL)
+            .build();
+
+        String testLogin = "scott";
+
+        Ignite ignite0 = startGrid("node0", testLogin, "", permSet0);
+        Ignite ignite1 = startGrid("node1", testLogin, "", permSet1);
+
+        ignite0.cluster().active(true);
+
+        IgniteCache<String, String> cache1 = ignite1.getOrCreateCache(DEFAULT_CACHE_NAME);
+
+        cache1.put("kInitRemove", "vInitRemove");
+        cache1.put("kExecRemove", "vExecRemove");
+        cache1.put("kInvokeRemove", "vInvokeRemove");
+        cache1.put("kCancelRemove", "vCancelRemove");
+
+        IgniteServices svcs = localServices(ignite0);
+
+        svcs.deployNodeSingleton(derivingTestSrvcName, new DerivingTestServiceImpl());
+
+        assertTrue(GridTestUtils.waitForCondition(() -> DerivingTestServiceImpl.executed, 3000));
+
+        Runnable svcProxy = svcs.serviceProxy(derivingTestSrvcName, Runnable.class, true);
+
+        svcProxy.run();
+
+        svcs.cancel(derivingTestSrvcName);
+        assertTrue(GridTestUtils.waitForCondition(() -> DerivingTestServiceImpl.cancelled, 3000));
+
+        assertNull("IMDBGG-1483", cache1.get("kInitPut"));
+        assertNull("IMDBGG-1617", cache1.get("kExecPut"));
+        assertNull(cache1.get("kInvokePut"));
+        assertNull("IMDBGG-1483", cache1.get("kCancelPut"));
+
+        assertEquals("IMDBGG-1483", "vInitRemove", cache1.get("kInitRemove"));
+        assertEquals("IMDBGG-1617", "vExecRemove", cache1.get("kExecRemove"));
+        assertEquals("vInvokeRemove", cache1.get("kInvokeRemove"));
+        assertEquals("IMDBGG-1483", "vCancelRemove", cache1.get("kCancelRemove"));
+
+        stopAllGrids();
+    }
+
+    /**
+     *
+     */
+    private static IgniteServices localServices(Ignite ignite) {
+        return ignite.services(ignite.cluster().forLocal());
+    }
+
+    /**
+     *
+     */
+    private static IgniteServices remoteServices(Ignite ignite) {
+        return ignite.services(ignite.cluster().forRemotes());
     }
 }
